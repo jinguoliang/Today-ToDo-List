4dbcdcfc0102c0c1d416c6aeb60eb36dda359422
Sergey Mironov <mironov@cps.com>
2012-10-12 15:34:48 +0400

    Изменена иконка checbox'ов отображающих состояние задач.

--- gen/com/cyberprophets/todaytodolist/R.java
+++ gen/com/cyberprophets/todaytodolist/R.java
@@ -37,14 +37,17 @@ public final class R {
         public static final int padding_small=0x7f050000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_add_task=0x7f020001;
-        public static final int ic_arrow_left=0x7f020002;
-        public static final int ic_arrow_right=0x7f020003;
-        public static final int ic_delete_task=0x7f020004;
-        public static final int ic_edit_task=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int ic_note=0x7f020007;
+        public static final int checkbox=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_add_task=0x7f020002;
+        public static final int ic_arrow_left=0x7f020003;
+        public static final int ic_arrow_right=0x7f020004;
+        public static final int ic_checkbox_checked=0x7f020005;
+        public static final int ic_checkbox_unchecked=0x7f020006;
+        public static final int ic_delete_task=0x7f020007;
+        public static final int ic_edit_task=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int ic_note=0x7f02000a;
     }
     public static final class id {
         public static final int add_category_button=0x7f09000e;
